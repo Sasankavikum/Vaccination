befe6904caa226dffd50288bf8d5dcb1ee3d988a
Dulitha Kaluarachchi <88882564+dulitha98@users.noreply.github.com>
2022-05-08 16:47:04 +0530

    Create 1VaccineReport.java
    report

---
+++ src/com/rcss/model/1VaccineReport.java
@@ -0,0 +1,26 @@
+package com.rcss.model;
+
+public class VaccineReport {
+	private String vaccineName;
+	private int count;
+	public VaccineReport() {
+		super();
+	}
+	public VaccineReport(String vaccineName, int count) {
+		super();
+		this.vaccineName = vaccineName;
+		this.count = count;
+	}
+	public String getVaccineName() {
+		return vaccineName;
+	}
+	public void setVaccineName(String vaccineName) {
+		this.vaccineName = vaccineName;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+}
